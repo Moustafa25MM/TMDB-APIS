1a18781135a11c7b804adfa409631e14ae37893d
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2024-05-12 02:50:39 +0300

    added the needed scripts to run the server

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { Express, Response, Request } from 'express';
+import * as dotenv from 'dotenv';
+import morgan from 'morgan';
+
+dotenv.config();
+
+const app: Express = express();
+app.use(express.json());
+app.use(morgan('tiny'));
+
+app.use('/', (req: Request, res: Response) => {
+  return res.status(200).send('Hello from Node TS');
+});
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`The server is running on port " ${port}"`);
+});
