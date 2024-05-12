8743a10d385c91546fbbf382bd5ba84f7d18fa2e
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2024-05-12 03:09:00 +0300

    Using Prisma ORM

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import express, { Express, Response, Request } from 'express';
 import * as dotenv from 'dotenv';
 import morgan from 'morgan';
+import { client } from './database/client';
 
 dotenv.config();
 
 const app: Express = express();
 app.use(express.json());
 app.use(morgan('tiny'));
+
+client.$connect().then(() => {
+  console.log('Successfully Connected to Database.');
+});
+
 
 app.use('/', (req: Request, res: Response) => {
   return res.status(200).send('Hello from Node TS');
