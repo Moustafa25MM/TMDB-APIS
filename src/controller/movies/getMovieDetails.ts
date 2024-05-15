bb926fdfa5708b29121630219bdbbfbed5baa873
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2024-05-15 06:16:52 +0300

    Optimize movie details fom api retrieval with Redis caching

--- src/controller/movies/getMovieDetails.ts
+++ src/controller/movies/getMovieDetails.ts
@@ -4,6 +4,7 @@ import { HttpException, NOT_FOUND, SERVER_ERROR } from '../../core/exceptions';
 import { z } from 'zod';
 import requestHandler from '../../core/handlers/requestHandler';
 import { validationHandler } from '../../core/handlers/validationHandler';
+import { redisClient } from '../../config/redisClient';
 
 const movieDetailsParamsSchema = z.object({
     title: z.string().min(1, "Title must not be empty")
@@ -13,13 +14,25 @@ export const getMovieDetailsByTitle = async (req: Request, res: Response, next: 
     const { title } = req.params;
 
     try {
+        const cacheKey = `movieDetails:${title}`;
+        const cachedData = await redisClient.get(cacheKey);
+        if (cachedData) {
+            return res.status(200).json({
+                source: 'cache',
+                movie: JSON.parse(cachedData)
+            });
+        }
+
         const searchedMovie = await searchMoviesByTitle(title);
         if (!searchedMovie) {
             return res.status(404).send({ message: 'Movie not found' });
         }
 
         const movieDetails = await fetchMovieDetails(searchedMovie.id);
+        await redisClient.setex(cacheKey, 3600, JSON.stringify(movieDetails));
+
         return requestHandler.sendSuccess(res)({
+            source: 'api',
             movie: movieDetails
         });
     } catch (error) {
